9bd97cf8d9e517a7c26c91f8f6a4fad20b9dc576
anders520 <anderschoy0604@gmail.com>
2021-10-11 17:28:18 -0700

    Added package with updated classes in roadrunner for tankdrive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tankdrive/opmode/DistanceTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.roadrunner.tankdrive.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp
+public class DistanceTest extends LinearOpMode{
+    DistanceSensor distance;
+    DcMotor motor;
+
+    @Override
+    public void runOpMode() {
+        // Get the distance sensor and motor from hardwareMap
+        distance = hardwareMap.get(DistanceSensor.class, "Distance");
+        //motor = hardwareMap.get(DcMotor.class, "Motor");
+
+        // Loop while the Op Mode is running
+        waitForStart();
+        while (opModeIsActive()) {
+            // If the distance in centimeters is less than 10, set the motor power to 0.3
+            if (distance.getDistance(DistanceUnit.CM) < 10) {
+                //motor.setPower(0.3);
+                telemetry.addData("In Range: ", "Yes");
+            } else { // Otherwise, stop the motor
+                //motor.setPower(0);
+                telemetry.addData("In Range: ", "No");
+            }
+            telemetry.update();
+        }
+    }
+}
