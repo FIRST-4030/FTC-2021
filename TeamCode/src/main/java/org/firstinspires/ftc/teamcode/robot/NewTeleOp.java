6c7ec13c60b94b5272c17c27ae21a768528f5998
anders520 <anderschoy0604@gmail.com>
2021-11-10 17:43:24 -0800

    Changes to NewTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/NewTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/NewTeleOp.java
@@ -33,9 +33,12 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @Config
 @TeleOp(name = "NewTeleOp", group = "Test")
@@ -47,20 +50,38 @@ public class NewTeleOp extends OpMode {
     private DcMotor depBelt = null;
     private Servo depLow = null;
     private Servo depMid = null;
+    private Servo depTilt = null;
     private DcMotor collector = null;
+    private Servo collectorArm = null;
+    private Servo capstoneArm = null;
+    //private Servo capstoneHook = null;
+    private DistanceSensor distanceLeft;
+    private DistanceSensor distanceRight;
 
     // Constants used for hardware
-    private static double DUCK_POWER = 0;
+    private static double DUCK_POWER = 0.0;
     private static double DEP_BELT_POWER = 1;
-    private static double LOW_OPEN = 0.9;
-    private static double LOW_CLOSE = 0.5;
+    private static double DEP_DOWN = 0.44;
+    private static double LOW_OPEN = 1;
+    private static double LOW_CLOSE = 0.56;
     private static double MID_OPEN = 0.9;
-    private static double MID_CLOSE = 0.4;
-    private static double COLLECTOR_POWER = 0.5;
-    private static double timerRatio = 0;
+    private static double MID_CLOSE = 0.49;
+    private static double COLLECTOR_UP = 0.75;
+    private static double COLLECTOR_DOWN = 0.0;
+    private static double COLLECTOR_POWER = -1;
+    private static double timerRatio = 0.0;
     private static double duckPowerMin = 0.65;  // min duck spinner speed (0 - 1.0)
     private static double duckPowerMax = 0.9;  // max duck spinner speed (0 - 1.0)
     private static double duckRampTime = 1.25;  // duck spinner ramp time (seconds, >0)
+    private static double CAP_UP = 0.25;
+    private static double CAP_MID = 0.5;
+    private static double CAP_DOWN = 0.75;
+    //private static double CAP_HOOK_DOWN = 0.75;
+    //private static double CAP_HOOK_UP = 0.25;
+
+    // Servo position test constants
+    private float servoPos = 0.5f;
+    private static final float INCREMENT = 0.01f;
 
     // Members
     private ElapsedTime runtime = new ElapsedTime();
@@ -92,9 +113,10 @@ public class NewTeleOp extends OpMode {
 
         // Depositor
         try {
-            depBelt = hardwareMap.get(DcMotor.class, "Dbelt");
-            depLow = hardwareMap.get(Servo.class, "Dlow");
-            depMid = hardwareMap.get(Servo.class, "Dmid");
+            depBelt = hardwareMap.get(DcMotor.class, "Depbelt");
+            depLow = hardwareMap.get(Servo.class, "Deplow");
+            depMid = hardwareMap.get(Servo.class, "Depmid");
+            depTilt = hardwareMap.get(Servo.class, "Deptilt");
         } catch (Exception e) {
             telemetry.log().add("Could not find depositor");
             error = true;
@@ -104,6 +126,25 @@ public class NewTeleOp extends OpMode {
         // Collector
         try {
             collector = hardwareMap.get(DcMotor.class, "Collector");
+            collectorArm = hardwareMap.get(Servo.class, "CollectorArm");
+        } catch (Exception e) {
+            telemetry.log().add("Could not find collector");
+            error = true;
+        }
+
+        // Capstone Grabber
+        try {
+            //capstoneHook = hardwareMap.get(Servo.class, "Caphook");
+            capstoneArm = hardwareMap.get(Servo.class, "Caparm");
+        } catch (Exception e) {
+            telemetry.log().add("Could not find collector");
+            error = true;
+        }
+
+        // Distance Sensors
+        try {
+            distanceLeft = hardwareMap.get(DistanceSensor.class, "DL");
+            distanceRight = hardwareMap.get(DistanceSensor.class, "DR");
         } catch (Exception e) {
             telemetry.log().add("Could not find collector");
             error = true;
@@ -130,6 +171,11 @@ public class NewTeleOp extends OpMode {
         leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        depTilt.setPosition(DEP_DOWN);
+        depLow.setPosition(LOW_CLOSE);
+        depMid.setPosition(MID_CLOSE);
+        capstoneArm.setPosition(0.5);
+        collectorArm.setPosition(COLLECTOR_DOWN);
     }
 
     @Override
@@ -168,11 +214,41 @@ public class NewTeleOp extends OpMode {
         }
 
         // Collector
-        if (gamepad1.left_bumper) {
+        if (gamepad1.right_bumper) {
             collector.setPower(COLLECTOR_POWER);
         } else {
             collector.setPower(0);
         }
+        if (gamepad1.left_bumper) {
+            collectorArm.setPosition(COLLECTOR_UP);
+        } else {
+            collectorArm.setPosition(COLLECTOR_DOWN);
+        }
+
+        // Capstone
+        if (gamepad2.dpad_up) {
+            capstoneArm.setPosition(CAP_UP);
+        }
+        if (gamepad2.left_bumper) {
+            capstoneArm.setPosition(CAP_MID);
+        }
+        if (gamepad2.right_bumper) {
+            capstoneArm.setPosition(CAP_DOWN);
+        }
+
+        // Distance Sensor
+        if (distanceLeft.getDistance(DistanceUnit.INCH) <= 19) {
+            telemetry.addData("In Range (Left): ", "Yes");
+        } else {
+            telemetry.addData("In Range (Left): ", "No");
+        }
+        if (distanceRight.getDistance(DistanceUnit.INCH) <= 19) {
+            telemetry.addData("In Range (Right): ", "Yes");
+        } else {
+            telemetry.addData("In Range (Right): ", "No");
+        }
+        telemetry.addData("Left Distance", String.format("%.01f in", distanceLeft.getDistance(DistanceUnit.INCH)));
+        telemetry.addData("Right Distance", String.format("%.01f in", distanceRight.getDistance(DistanceUnit.INCH)));
 
         // Feedback
         telemetry.addData("Drive", "L %.2f/%d, R %.2f/%d",
@@ -182,6 +258,20 @@ public class NewTeleOp extends OpMode {
                 duckSpinner.getPower(), collector.getPower());
         telemetry.addData("Depositor", "B %.2f, L %.2f, M %.2f",
                 depBelt.getPower(), depLow.getPosition(), depMid.getPosition());
+        /*
+        // Shows number of servoPos
+        telemetry.addData("Pos:", servoPos);
+        // Moving the servo position and number should increase
+        if (gamepad1.dpad_up) {
+            servoPos += INCREMENT;
+            servoPos = Math.min(1.0f, servoPos);
+            // Moving the servo position and number should decrease
+        } else if (gamepad1.dpad_down) {
+            servoPos -= INCREMENT;
+            servoPos = Math.max(0.0f, servoPos);
+        }
+        // Set position of desired servo
+        depMid.setPosition(servoPos);*/
     }
 
     @Override
