a794ba9f4612d7c69b6501b15f1cafdbb2a7ba7d
icaras84 <icaras84@gmail.com>
2022-04-01 17:46:54 -0700

    Adapted test to a circle follower (theoretically)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/rrImpl/TankDriveTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/rrImpl/TankDriveTesting.java
@@ -4,9 +4,11 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.tfodohm.TFMaths.TFMathExtension;
 import org.firstinspires.ftc.teamcode.utils.LoopUtil;
 
 @Config
@@ -23,7 +25,9 @@ public class TankDriveTesting extends LoopUtil {
     //poses
     public static Pose2d startingPose = new Pose2d(0, 0);
     public static double DISTANCE = 30;
-    //public static Vector2d splineTarget = new Vector2d(9, 9);
+    public static double angleOffset = TFMathExtension.pi/4;
+    public static double trackedHeading = 0;
+    public static Vector2d splineTarget = new Vector2d(9, 9);
 
     @Override
     public void opInit() {
@@ -57,16 +61,15 @@ public class TankDriveTesting extends LoopUtil {
 
     @Override
     public void opUpdate(double deltaTime) {
+        trackedHeading = drive.getRawExternalHeading() + angleOffset;
         Trajectory trajectory = drive.trajectoryBuilder(startingPose)
-                .forward(DISTANCE)
+                .splineTo(splineTarget, trackedHeading)
                 .build();
-
-        drive.followTrajectory(trajectory);
-        drive.turn(Math.toRadians(-90));
 
         startingPose = trajectory.end().plus(new Pose2d(0, 0, Math.toRadians(-90)));
         estimatedPaths.record(startingPose);
         currentPaths.record(drive.getLastError());
+        splineTarget.rotated(angleOffset);
     }
 
     @Override
